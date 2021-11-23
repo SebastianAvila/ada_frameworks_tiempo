ae6f9b43de3a7911534439f5773ce8291a59a72d
SebastianAvila <jesusaviala@gmail.com>
2021-11-23 16:49:58 -0600

    Se descargo el countdown y se elimino el react pri

---
+++ countdown-timer-react/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import CountDownTimer from './CountDownTimer';
+
+function App() {
+
+  const hoursMinSecs = {hours:1, minutes: 20, seconds: 40}
+  
+  return (
+    <div className="App">
+        <CountDownTimer hoursMinSecs={hoursMinSecs}/>
+    </div>
+  );
+}
+
+export default App;
